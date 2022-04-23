6581985feee3f6e5e7f2c6d93483ea9cf00955ab
medzdev <medzdeveloper@gmail.com>
2022-04-23 12:09:47 -0300

    novos exercicios

---
+++ Variable Declaration/CollegeProfileVariables.java
@@ -0,0 +1,30 @@
+/*You are asked to develop a software for a college. 
+The system shall maintain information about students. 
+This includes name, address, phone number, gender, date of birth, height, weight, 
+degree pursued (e.g., B.Sc., B.A.), year of study, average GPA, with/without tuition grant, 
+is/is not a scholar. Each student is assigned a unique 8-digit number as id.
+You are required to identify the variables, assign a suitable name to each variable and choose an appropriate type. 
+Write the variable declaration statements as in the above example.
+*/
+
+public class CollegeProfileVariables {
+    public static void main (String[] args) {
+        String studentName;
+        String address;
+        int phoneNumber; // 55119********
+        char gender; // m, f, n.
+        int birthDay; // YYYYMMDD
+        int height; // in centimeters
+        double weight;
+        String degree;
+        int yearClass;
+        double avgGPA;
+        boolean tuitionGrant;
+        boolean scholar;
+        int studentID; //******** (8 number)
+
+
+
+
+    }
+}
--- Weather Conversion/Celsius2Fahrenheit.java
+++ Weather Conversion/Celsius2Fahrenheit.java
@@ -1,3 +1,8 @@
-public    class Celsius2Fahrenheit {
-    
+public class Celsius2Fahrenheit {
+    public static void main(String[] args) {
+        int fahrenheit, celsius = 25;
+        fahrenheit = (celsius*9/5) + 32;
+        System.out.println(celsius + " graus C° convertido em F é: " + fahrenheit);
+        
+    }
 }
