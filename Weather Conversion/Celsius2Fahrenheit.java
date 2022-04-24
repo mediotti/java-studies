a5836e412ca8ce0313a493371e496a639e8372f6
medzdev <medzdeveloper@gmail.com>
2022-04-24 20:57:54 -0300

    aaa

--- Weather Conversion/Celsius2Fahrenheit.java
+++ Weather Conversion/Celsius2Fahrenheit.java
@@ -1,3 +1,8 @@
-public    class Celsius2Fahrenheit {
+public class Celsius2Fahrenheit {
+    public static void main (String[] args) {
+        int fahrenheit = 0, celsius = 0;
+        fahrenheit = 
+
+    }
     
 }
